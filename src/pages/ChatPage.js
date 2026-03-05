f404d9b460111d336a7fb218bca92c08c43f1d18
stephen99-dot <hello@crmwizardai.com>
2026-03-05 08:04:07 +0000

    Update ChatPage.js

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -649,7 +649,7 @@ export default function ChatPage() {
                 <path d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
               </svg>
             </button>
-            <input ref={fileInputRef} type="file" multiple onChange={e => { addFiles(e.target.files); e.target.value = ''; }} style={{ display: 'none' }} accept=".pdf,.png,.jpg,.jpeg,.gif,.webp,.zip" />
+            <input ref={fileInputRef} type="file" multiple onChange={e => { if (e.target.files && e.target.files.length > 0) { addFiles(e.target.files); } e.target.value = ''; }} style={{ display: 'none' }} accept=".pdf,.png,.jpg,.jpeg,.gif,.webp,.zip" />
             <textarea
               className="aiqs-chat-textarea"
               value={input} onChange={e => setInput(e.target.value)} onKeyDown={handleKeyDown}
