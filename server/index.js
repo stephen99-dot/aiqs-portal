b51411b17469262c7d9b193ec64e605e09689485
stephen99-dot <hello@crmwizardai.com>
2026-04-14 10:34:36 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors({ origin: true, credentials: true }));
 app.post('/api/stripe/webhook', express.raw({ type: 'application/json' }), stripeWebhook);
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
 app.use(cookieParser());
 app.use('/api', routes);
 app.use('/api', chatRoutes);
